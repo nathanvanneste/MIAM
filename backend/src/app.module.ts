2ce2c13dcfc5c8fce55e2facf10096f0d3141bea
wbarran <barran.william@gmail.com>
2026-04-27 21:25:34 +0200

    modification du nom group en groups

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { IngredientsModule } from './ingredients/ingredients.module';
 import { ShoppingListsModule } from './shopping-lists/shopping-lists.module';
 import { GroupModule } from './group/group.module';
 import { UsersModule } from './users/users.module';
+import { GroupsModule } from './groups/groups.module';
 
 
 @Module({
-  imports: [RecipesModule, IngredientsModule, ShoppingListsModule, GroupModule, UsersModule],
+  imports: [RecipesModule, IngredientsModule, ShoppingListsModule, GroupModule, UsersModule, GroupsModule],
 })
 export class AppModule { }
--- backend/src/groups/groups.controller.ts
+++ backend/src/groups/groups.controller.ts
@@ -1,34 +1,34 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { GroupService } from './group.service';
+import { GroupsService } from './groups.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 
-@Controller('group')
-export class GroupController {
-  constructor(private readonly groupService: GroupService) {}
+@Controller('groups')
+export class GroupsController {
+  constructor(private readonly groupsService: GroupsService) {}
 
   @Post()
   create(@Body() createGroupDto: CreateGroupDto) {
-    return this.groupService.create(createGroupDto);
+    return this.groupsService.create(createGroupDto);
   }
 
   @Get()
   findAll() {
-    return this.groupService.findAll();
+    return this.groupsService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.groupService.findOne(+id);
+    return this.groupsService.findOne(+id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto) {
-    return this.groupService.update(+id, updateGroupDto);
+    return this.groupsService.update(+id, updateGroupDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.groupService.remove(+id);
+    return this.groupsService.remove(+id);
   }
 }
