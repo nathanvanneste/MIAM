41408d3ad25dab83aa9404e186255f39fdc2c678
wbarran <barran.william@gmail.com>
2026-04-27 21:38:59 +0200

    correction d'un import group en groups

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { RecipesModule } from './recipes/recipes.module';
 import { IngredientsModule } from './ingredients/ingredients.module';
 import { ShoppingListsModule } from './shopping-lists/shopping-lists.module';
-import { GroupModule } from './group/group.module';
-import { UsersModule } from './users/users.module';
 import { GroupsModule } from './groups/groups.module';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
-  imports: [RecipesModule, IngredientsModule, ShoppingListsModule, GroupModule, UsersModule, GroupsModule],
+  imports: [RecipesModule, IngredientsModule, ShoppingListsModule, GroupsModule, UsersModule],
 })
 export class AppModule { }
