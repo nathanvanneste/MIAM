3eddd36a6dfe7a8777624a5e79c08399da1ef03a
root <root@DESKTOP-LO334AE.>
2026-04-29 12:40:06 +0200

    debut profilescreen

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,10 @@
 import WelcomeScreen from "@/src/screens/WelcomeScreen";
 
 import { Text, View } from "react-native";
+import ProfileScreen from "../src/screens/ProfileScreen";
 
 export default function Index() {
   return (
-    <WelcomeScreen />
+    <ProfileScreen />
   );
 }
