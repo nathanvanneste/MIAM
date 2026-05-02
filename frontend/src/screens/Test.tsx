0a286b69905504af7b9ea54b8c0d5a1fbf5383e2
ElodieDuverger <elodie.duverger@insa-lyon.fr>
2026-05-02 16:11:22 +0200

    update to main

--- frontend/src/screens/Test.tsx
+++ frontend/src/screens/Test.tsx
@@ -104,7 +104,7 @@ const groups = [
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: COLORS.background,
+    backgroundColor: Colors.background,
   },
 
   content: {
