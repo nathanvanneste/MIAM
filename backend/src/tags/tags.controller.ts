4171ef981a4e9c99c7eedd3dff994e08514c6452
AshTirou <ash210trvl@orange.fr>
2026-05-03 18:45:38 +0200

    Tags Routes Protections

--- backend/src/tags/tags.controller.ts
+++ backend/src/tags/tags.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { TagsService } from './tags.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
